8ef57a58a49e15010c69c645a4b9a7c58ebd5091
Pavel Drovnin <pavel98rus@gmail.com>
2024-01-20 01:41:55 +0300

    update schemas

--- app/plan/schemas.py
+++ app/plan/schemas.py
@@ -1,5 +1,35 @@
-# Модельки
+# Модели для pydantic и для sqlalchemy
+# Используйте SQLModel!!!!!  https://sqlmodel.tiangolo.com/tutorial/fastapi/
+from sqlmodel import SQLModel
+
+
+class User():
+    pass
 
 
 class Employee():
+    pass
+
+
+class Links():
+    pass
+
+
+class Comments():
+    pass
+
+
+class Attachments():
+    pass
+
+
+class IPR():
+    pass
+
+
+class Task():
+    pass
+
+
+class Task_In_IPR():
     pass
