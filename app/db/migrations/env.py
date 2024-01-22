cc1b6de2683d82764b6a6b66129ec520bbf21974
Ivan Pavlov <ivnpvl@disroot.org>
2024-01-22 10:13:06 +0300

    Fix config_file_name getter in env.py

--- app/db/migrations/env.py
+++ app/db/migrations/env.py
@@ -11,7 +11,7 @@ from app.plan.schemas import *
 
 config = context.config
 
-if config.config_file_name is not None:
+if config.config_file_name:
     fileConfig(config.config_file_name)
 
 # Через config можно переопределять параметры, заданные а alembic.ini.
