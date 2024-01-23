b0e5748115aa852cef0ece86e0e775f57c574ba8
Pavel Drovnin <pavel98rus@gmail.com>
2024-01-23 19:24:35 +0300

    fix parametr

--- app/app_config.py
+++ app/app_config.py
@@ -11,8 +11,8 @@ DEBUG можно использовать для разнообразной ло
 class PostgresSettings(BaseSettings):
     DB_HOST: str = os.getenv("DB_HOST", "localhost")
     DB_PORT: int = os.getenv("DB_PORT", 5432)
-    DB_USER: str = os.getenv("DB_USER", "postgres")
-    DB_PASS: str = os.getenv("DB_PASS", "postgres")
+    DB_USER: str = os.getenv("POSTGRES_USER", "postgres")
+    DB_PASS: str = os.getenv("POSTGRES_PASSWORD", "postgres")
     DB_NAME: str = os.getenv("DB_NAME", "localhost")
 
     @property
