08641206a8162b314c5a588370f27ccbe9240967
Pavel Drovnin <pavel98rus@gmail.com>
2024-01-28 06:38:44 +0300

    utils for task

--- app/api/v1/endpoints/task.py
+++ app/api/v1/endpoints/task.py
@@ -2,11 +2,16 @@
 from http import HTTPStatus
 
 from fastapi import APIRouter, Depends
+from datetime import datetime
+from typing import Union
 
-from api.v1 import openapi
+from api.v1 import openapi, validators
 from core.logger import logger_factory
+from db.crud import task_crud, plan_crud, comment_crud
 from db.database import AsyncSession, get_async_session
-from schemas.task import TaskCreate, TaskRead, TaskUpdate
+from schemas.task import TaskCreate, TaskRead, TaskReadWithComments, TaskUpdate, TaskStatus
+from schemas.plan import PlanStatus
+from schemas.comment import CommentType
 from services.user import User, get_user
 
 logger = logger_factory(__name__)
@@ -15,8 +20,8 @@ router = APIRouter(prefix="/plans/{plan_id}/tasks")
 
 
 @router.get(
-    '/{task_id}',
-    response_model=TaskRead,
+    "/{task_id}",
+    response_model=TaskReadWithComments,
     **openapi.task.get_task.model_dump()
 )
 async def get_task(
@@ -25,12 +30,30 @@ async def get_task(
         user: User = Depends(get_user),
         session: AsyncSession = Depends(get_async_session),
 ):
-    """Получение задачи по id."""
-    pass
+    """Получение задачи по id. Проверка и изменение статуса задачи и
+    статуса плна"""
+    await validators.check_task_and_user_access(task_id, user.id, session)
+    task = await task_crud.get(
+        session, {'id': task_id}
+    )
+    # Проверка статуса задачи и изменение статуса задачи и статуса плана
+    if task.status == TaskStatus.CREATED and user.supervisor_id:
+        await task_crud.update(
+            session,
+            {'id': task_id},
+            {'status': TaskStatus.IN_PROGRESS}
+        )
+        await plan_crud.update(
+            session,
+            {'id': plan_id},
+            {"status": PlanStatus.IN_PROGRESS}
+        )
+    task.status = TaskStatus.IN_PROGRESS
+    return task
 
 
 @router.get(
-    '/',
+    "/",
     response_model=list[TaskRead],
     **openapi.task.get_tasks.model_dump()
 )
@@ -40,11 +63,12 @@ async def get_tasks(
         session: AsyncSession = Depends(get_async_session),
 ):
     """Получение списка задач."""
-    pass
+    await validators.check_plan_and_user_access(plan_id, user.id, session)
+    return await task_crud.get_all(session, {'plan_id': plan_id})
 
 
 @router.post(
-    '/',
+    "/",
     response_model=TaskRead,
     **openapi.task.create_task.model_dump()
 )
@@ -54,13 +78,30 @@ async def create_task(
         user: User = Depends(get_user),
         session: AsyncSession = Depends(get_async_session),
 ):
-    """Создание задачи."""
-    pass
+    """Создание задачи. Добавление нового нового комментарияк задаче."""
+    await validators.check_plan_and_user_access(plan_id, user.id, session)
+    await validators.check_role(user)
+    task = await task_crud.create(
+        session, {
+        **task_create.model_dump(),
+        "plan_id": plan_id
+        }
+    )
+    # Добавление комментария
+    await comment_crud.create(session, {
+        "task_id": task.id,
+        "author_id": user.id,
+        "type": CommentType.TEXT,
+        "content": "Задача создана {}".format(
+            datetime.today().strftime('%d.%m.%Y')
+        )
+    })
+    return task
 
 
 @router.patch(
-    '/{task_id}',
-    response_model=TaskRead,
+    "/{task_id}",
+    response_model=Union[TaskRead, list[TaskRead]],
     **openapi.task.update_task.model_dump()
 )
 async def update_task(
@@ -71,11 +112,20 @@ async def update_task(
         session: AsyncSession = Depends(get_async_session),
 ):
     """Обновление задачи."""
-    pass
+    await validators.check_plan_and_user_access(plan_id, user.id, session)
+    await validators.check_role(user)
+    new_task = await task_crud.update(
+        session,
+        {"id": task_id},
+        task_patch.model_dump(exclude_unset=True)
+    )
+    if len(new_task) > 1:
+        return new_task
+    return new_task[0]
 
 
 @router.delete(
-    '/{task_id}',
+    "/{task_id}",
     status_code=HTTPStatus.NO_CONTENT,
     **openapi.task.delete_task.model_dump()
 )
@@ -86,4 +136,6 @@ async def delete_task(
         session: AsyncSession = Depends(get_async_session),
 ):
     """Удаление задачи."""
-    pass
+    await validators.check_plan_and_user_access(plan_id, user.id, session)
+    await validators.check_role(user)
+    await task_crud.delete(session, {"id": task_id})
