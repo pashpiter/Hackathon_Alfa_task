48d2b2371cf1e6651ce37e3d04b0a65bbfc4fe16
Pavel Drovnin <pavel98rus@gmail.com>
2024-02-03 07:34:14 +0300

    create_many crud

--- app/db/crud/notification.py
+++ app/db/crud/notification.py
@@ -1,7 +1,7 @@
 from sqlalchemy import update
 
 from core.logger import logger_factory
-from db.crud.base import CRUDBase
+from db.crud.base import CRUDBase, ModelType
 from db.database import AsyncSession
 from schemas.base import PK_TYPE, USER_PK_TYPE
 from schemas.notification import Notification
@@ -29,6 +29,24 @@ class CRUDNotification(CRUDBase):
         )
         await session.execute(query)
         await session.commit()
+
+    async def create_many(
+            self,
+            session: AsyncSession,
+            objs_in: list[Notification],
+    ) -> ModelType:
+        """Создаёт в БД записи из полученных объектов."""
+        self.logger.debug(
+            f'CREATE_MANY: model={self.model}, objs_in={objs_in}')
+
+        if not (isinstance(objs_in, list)):
+            msg = f'Source object should be of types: list or {self.model}'
+            self.logger.error(msg)
+            raise ValueError(msg)
+
+        session.add_all(objs_in)
+        await session.commit()
+        await session.refresh(objs_in)
 
 
 notification_crud = CRUDNotification(Notification, logger_factory(__name__))
